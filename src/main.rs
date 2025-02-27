f77f40c7d92e204b1659138daa5e30131de64eee
Marcin Witek <witek.marcin96@gmail.com>
2025-02-27 22:15:55 +0100

    Implemented proper input handling

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,21 @@
-use crossterm::event::{self, Event, KeyCode};
-use crossterm::terminal::{Clear, ClearType};
-use crossterm::ExecutableCommand;
+use crossterm::event::{self, Event, KeyCode, KeyEventKind};
 use std::io::{self};
+use std::time::Duration;
 
 fn main() -> Result<(), io::Error> {
-    let mut stdout = io::stdout();
-    let mut input = String::new();
-
-    // Main loop to handle events and input
     'main: loop {
-        // Clear the terminal
-        stdout.execute(Clear(ClearType::All))?;
-        println!("Rusty ToDo List!");
-        println!("Type something or press ESC to quit.\n");
+        if event::poll(Duration::default())? {
+            if let Event::Key(key) = event::read()? {
+                if key.kind == KeyEventKind::Release {
+                    match key.code {
+                        KeyCode::Esc => break 'main,
+                        KeyCode::Char(c) => {
+                            eprint!("{}", c)
+                        }
 
-        if !input.is_empty() {
-            println!("{}", input);
-        }
-
-        if let Event::Key(key) = event::read()? {
-            match key.code {
-                KeyCode::Esc => break 'main, // Exit the program
-                KeyCode::Enter => input.clear(),
-                KeyCode::Char(c) => {
-                    // Append typed characters to the input
-                    input.push(c);
+                        _ => {}
+                    }
                 }
-                KeyCode::Backspace => {
-                    // Handle backspace (remove last character)
-                    input.pop();
-                }
-                _ => (),
             }
         }
     }
