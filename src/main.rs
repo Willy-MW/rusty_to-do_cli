d2be212f97e8221af454735a5e1b41aa69d77863
Marcin Witek <witek.marcin96@gmail.com>
2025-02-27 22:29:51 +0100

    Refactored handling user input into separate function

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,32 @@
+use anyhow::Result;
 use crossterm::event::{self, Event, KeyCode, KeyEventKind};
-use std::io::{self};
 use std::time::Duration;
 
-fn main() -> Result<(), io::Error> {
+fn main() -> Result<()> {
     'main: loop {
-        if event::poll(Duration::default())? {
-            if let Event::Key(key) = event::read()? {
-                if key.kind == KeyEventKind::Release {
-                    match key.code {
-                        KeyCode::Esc => break 'main,
-                        KeyCode::Char(c) => {
-                            eprint!("{}", c)
-                        }
-
-                        _ => {}
-                    }
-                }
-            }
+        if !handle_user_input()? {
+            break 'main;
         }
     }
 
     Ok(())
+}
+
+// Function to handle user input
+fn handle_user_input() -> Result<bool> {
+    let mut result = Ok(true);
+
+    if event::poll(Duration::default())? {
+        if let Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Release {
+                match key.code {
+                    KeyCode::Esc => result = Ok(false),   // Exit the loop
+                    KeyCode::Char(c) => eprint!("{}", c), // Print input
+                    _ => {}
+                };
+            }
+        }
+    }
+
+    result
 }
