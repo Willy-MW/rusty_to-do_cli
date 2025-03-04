f0f7b05b6af1db4a48a2cd665f5c86322767adfc
Marcin Witek <witek.marcin96@gmail.com>
2025-03-04 23:02:18 +0100

    Added getting command and argument from input

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use anyhow::Result;
 use crossterm::event::{self, Event, KeyCode, KeyEventKind};
 
 fn main() -> Result<()> {
-    let mut command = String::new();
+    let mut input = String::new();
 
     'main: loop {
-        match handle_user_input(&mut command)? {
-            Some(Action::ProcessCommand) => {
-                process_command(&command)?;
-                command.clear();
+        match handle_user_input(&mut input)? {
+            Some(Action::ProcessInput) => {
+                process_input(&input.trim())?;
+                input.clear();
             }
             Some(Action::Exit) => break 'main,
             None => {}
@@ -19,7 +19,7 @@ fn main() -> Result<()> {
 }
 
 // Function to handle user input
-fn handle_user_input(command: &mut String) -> Result<Option<Action>> {
+fn handle_user_input(input: &mut String) -> Result<Option<Action>> {
     let mut action: Option<Action> = None;
 
     if let Event::Key(key) = event::read()? {
@@ -28,15 +28,15 @@ fn handle_user_input(command: &mut String) -> Result<Option<Action>> {
                 KeyCode::Esc => action = Some(Action::Exit),
                 KeyCode::Char(c) => {
                     eprint!("{}", c);
-                    (*command).push(c);
+                    (*input).push(c);
                 }
                 KeyCode::Backspace => {
                     eprint!("\x1b[D \x1b[D");
-                    (*command).pop();
+                    (*input).pop();
                 }
                 KeyCode::Enter => {
                     println!();
-                    action = Some(Action::ProcessCommand)
+                    action = Some(Action::ProcessInput)
                 }
                 _ => {}
             }
@@ -46,23 +46,51 @@ fn handle_user_input(command: &mut String) -> Result<Option<Action>> {
     Ok(action)
 }
 
-fn process_command(command: &str) -> Result<()> {
-    println!("Processing command: {}", command);
-
-    let Some(cmd) = command.split(' ').next() else {
-        return Ok(());
-    };
-
-    match cmd {
-        "complete" => println!("Complete"),
-        "delete" => println!("Delete"),
-        _ => println!("Unknown command: {}", cmd),
-    }
+fn process_input(input: &str) -> Result<()> {
+    let command = get_command(input);
+    let argument = get_argument(input, command.is_some());
 
     Ok(())
 }
 
+fn get_command(input: &str) -> Option<Command> {
+    let mut input_iter = input.split_whitespace();
+
+    let Some(command) = input_iter.next() else {
+        return None;
+    };
+
+    match command.to_lowercase().as_str() {
+        "/complete" => Some(Command::Complete),
+        "/undo" => Some(Command::Undo),
+        "/delete" => Some(Command::Delete),
+        _ => None,
+    }
+}
+
+fn get_argument(input: &str, is_command: bool) -> &str {
+    if is_command {
+        input.split_whitespace().nth(1).unwrap_or("")
+    } else {
+        input
+    }
+}
+
+fn complete_task(number: isize) {
+    println!("Completed task #{}", number);
+}
+
+fn delete_task(number: isize) {
+    println!("Deleted task #{}", number);
+}
+
 enum Action {
-    ProcessCommand,
+    ProcessInput,
     Exit,
+}
+
+enum Command {
+    Complete,
+    Undo,
+    Delete,
 }
