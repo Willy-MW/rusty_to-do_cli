a802181f048eceb8a97b5b8005a4d0cc3510cc70
Marcin Witek <witek.marcin96@gmail.com>
2025-03-06 22:34:35 +0100

    Created TaksList struct to manage created tasks. Refactored code to use it.

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,12 @@ static COUNTER: AtomicUsize = AtomicUsize::new(1);
 
 fn main() -> Result<()> {
     let mut input = String::new();
+    let mut task_list = TaskList::new();
 
     'main: loop {
         match handle_user_input(&mut input)? {
             Some(Action::ProcessInput) => {
-                process_input(&input.trim())?;
+                process_input(&input, &mut task_list)?;
                 input.clear();
             }
             Some(Action::Exit) => break 'main,
@@ -50,9 +51,22 @@ fn handle_user_input(input: &mut String) -> Result<Option<Action>> {
     Ok(action)
 }
 
-fn process_input(input: &str) -> Result<()> {
+fn process_input(input: &str, task_list: &mut TaskList) -> Result<()> {
     let command = get_command(input);
     let argument = get_argument(input, command.is_some());
+
+    if command.is_none() {
+        task_list.create_task(argument);
+        return Ok(());
+    }
+
+    let task_id = argument.parse().unwrap_or(0);
+
+    match command.unwrap() {
+        Command::Complete => task_list.complete_task(task_id),
+        Command::Undo => task_list.undo_task(task_id),
+        Command::Delete => task_list.delete_task(task_id),
+    }
 
     Ok(())
 }
@@ -60,9 +74,7 @@ fn process_input(input: &str) -> Result<()> {
 fn get_command(input: &str) -> Option<Command> {
     let mut input_iter = input.split_whitespace();
 
-    let Some(command) = input_iter.next() else {
-        return None;
-    };
+    let command = input_iter.next()?;
 
     match command.to_lowercase().as_str() {
         "/complete" => Some(Command::Complete),
@@ -76,28 +88,8 @@ fn get_argument(input: &str, is_command: bool) -> &str {
     if is_command {
         input.split_whitespace().nth(1).unwrap_or("")
     } else {
-        input
+        input.trim()
     }
-}
-
-fn create_task(description: &str) -> Option<Task> {
-    if description.is_empty() {
-        None
-    } else {
-        Some(Task::new(description))
-    }
-}
-
-fn complete_task(number: isize) {
-    println!("Completed task #{}", number);
-}
-
-fn undo_task(number: isize) {
-    println!("Undo task #{}", number);
-}
-
-fn delete_task(number: isize) {
-    println!("Deleted task #{}", number);
 }
 
 #[derive(PartialEq, Debug)]
@@ -130,5 +122,51 @@ impl Task {
 impl Display for Task {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
         write!(f, "{}. {}", self.id, self.description)
+    }
+}
+
+#[derive(PartialEq, Debug)]
+struct TaskList {
+    todo: Vec<Task>,
+    completed: Vec<Task>,
+}
+
+impl TaskList {
+    fn new() -> Self {
+        Self {
+            todo: Vec::new(),
+            completed: Vec::new(),
+        }
+    }
+
+    fn create_task(&mut self, description: &str) -> usize {
+        if description.is_empty() {
+            return 0;
+        }
+
+        let task = Task::new(description);
+        let task_id = task.id;
+
+        if self.todo.len() < task_id {
+            self.todo.push(task);
+        } else {
+            self.todo.insert(task_id - 1, task);
+        }
+
+        println!("Created task #{}", task_id);
+
+        task_id
+    }
+
+    fn complete_task(&mut self, number: usize) {
+        println!("Completed task #{}", number);
+    }
+
+    fn undo_task(&mut self, number: usize) {
+        println!("Undo task #{}", number);
+    }
+
+    fn delete_task(&mut self, number: usize) {
+        println!("Deleted task #{}", number);
     }
 }
