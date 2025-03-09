2226e8bea9bed44bdd072c6a70fce47ee628fd1e
Marcin Witek <witek.marcin96@gmail.com>
2025-03-09 14:22:16 +0100

    Added help command with explanation of all commands. Enhanced UI/UX

--- src/input.rs
+++ src/input.rs
@@ -3,6 +3,7 @@ use anyhow::Result;
 use crossterm::event::{Event, KeyCode, KeyEventKind};
 use crossterm::terminal::{Clear, ClearType};
 use crossterm::{event, execute};
+use std::fmt::Display;
 use std::io::stdout;
 
 #[derive(PartialEq, Debug)]
@@ -11,21 +12,60 @@ pub enum Action {
     Exit,
 }
 
-#[derive(PartialEq, Debug)]
+#[derive(PartialEq, Debug, Clone)]
 enum Command {
     Complete,
     Undo,
     Delete,
+    Help,
 }
 
 impl TryFrom<&str> for Command {
     type Error = &'static str;
     fn try_from(value: &str) -> Result<Self, Self::Error> {
         match value {
-            "/complete" => Ok(Command::Complete),
+            "/help" => Ok(Command::Help),
+            "/do" => Ok(Command::Complete),
             "/undo" => Ok(Command::Undo),
             "/delete" => Ok(Command::Delete),
             _ => Err("Unknown command"),
+        }
+    }
+}
+
+impl From<Command> for &'static str {
+    fn from(command: Command) -> Self {
+        match command {
+            Command::Help => "/help",
+            Command::Complete => "/complete",
+            Command::Undo => "/undo",
+            Command::Delete => "/delete",
+        }
+    }
+}
+
+impl Display for Command {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        let as_str: &str = self.clone().into();
+        write!(f, "{}", as_str)
+    }
+}
+
+impl Command {
+    fn get_all() -> Vec<Command> {
+        Vec::from([
+            Command::Help,
+            Command::Complete,
+            Command::Delete,
+            Command::Undo,
+        ])
+    }
+    pub fn get_description(&self) -> &str {
+        match self {
+            Command::Help => "Lists all commands.",
+            Command::Complete => "Marks task as completed and moves it into completed tasks list.",
+            Command::Undo => "Moves task back from completed tasks list into todo list.",
+            Command::Delete => "Deletes the task.",
         }
     }
 }
@@ -45,7 +85,10 @@ pub fn handle_user_input(input: &mut String) -> Result<Option<Action>> {
                     eprint!("\x1b[D \x1b[D");
                     (*input).pop();
                 }
-                KeyCode::Enter => action = Some(Action::ProcessInput),
+                KeyCode::Enter => {
+                    println!();
+                    action = Some(Action::ProcessInput)
+                }
                 _ => {}
             }
         }
@@ -66,6 +109,7 @@ pub fn process_input(input: &str, task_list: &mut TaskList) -> Result<()> {
     let task_id = argument.parse().unwrap_or(0);
 
     match command.unwrap() {
+        Command::Help => print_help(),
         Command::Complete => task_list.complete_task(task_id),
         Command::Undo => task_list.undo_task(task_id),
         Command::Delete => task_list.delete_task(task_id),
@@ -90,4 +134,17 @@ fn get_argument(input: &str, is_command: bool) -> &str {
 
 pub fn clear_screen() {
     execute!(stdout(), Clear(ClearType::All)).unwrap();
+}
+
+pub fn print_header() {
+    println!("Rust TODO list. Type something to add new task. Type /help for a list of available commands.");
+}
+
+pub fn print_help() {
+    for command in Command::get_all() {
+        print!("{}", command);
+        print!(" - ");
+        println!("{}", command.get_description());
+    }
+    println!("Press enter to continue...");
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use anyhow::Result;
-use to_do_cli::input::{clear_screen, handle_user_input, process_input, Action};
+use to_do_cli::input::{clear_screen, handle_user_input, print_header, process_input, Action};
 use to_do_cli::task_list::TaskList;
 
 fn main() -> Result<()> {
     let mut input = String::new();
     let mut task_list = TaskList::default();
 
+    print_header();
+
     'main: loop {
         match handle_user_input(&mut input)? {
             Some(Action::ProcessInput) => {
+                clear_screen();
                 process_input(&input, &mut task_list)?;
                 input.clear();
-                clear_screen();
+                print_header();
                 print!("{}", task_list);
             }
             Some(Action::Exit) => break 'main,
